5a1c38b4952fcef2fe9cefe7936f8de6ad82a254
João Pedro Barbosa <jpdsbarbosa@inf.ufpel.edu.br>
2025-03-28 15:00:59 -0300

    correção

--- daily_balance_noxpay.py
+++ daily_balance_noxpay.py
@@ -19,13 +19,7 @@ url_financial = os.getenv('url_financial')
 try:
     CONTAS_GRANDES = json.loads(os.getenv('CONTAS_GRANDES', '{}'))
 except json.JSONDecodeError:
-    print("Erro ao decodificar CONTAS_GRANDES. Usando configuração padrão.")
-    CONTAS_GRANDES = {
-        "15277CDE747846BB84C2DFCE85DB504B": {"timeout": 300, "retries": 5, "batch_size": 50},
-        "44B0F69654774D829A00413476711E1C": {"timeout": 300, "retries": 5, "batch_size": 50},
-        "AB3FF5EA035C48A5864F9B0C6DCC2CC4": {"timeout": 300, "retries": 5, "batch_size": 50},
-        "EA67B2F52FC342AB8D91E3293229FE0B": {"timeout": 300, "retries": 5, "batch_size": 50}
-    }
+    CONTAS_GRANDES = {}
 
 class RateLimiter:
     def __init__(self, max_requests=900, time_window=60):
